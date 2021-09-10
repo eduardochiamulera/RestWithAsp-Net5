a199b42b990b2893bcc5208a92991bdf02318977
Eduardo Antonio Fraga Chiamulera <eduardo.chiamulera@linx.com.br>
2021-09-10 17:17:00 -0300

    consultado dados no banco

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,68 +1,85 @@
 ﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace RestWithASPNETUdemy.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
-        private volatile int count;
+        private readonly MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        public Person FindById(long id)
+        {
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Persons.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return person;
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
             return person;
         }
 
         public void Delete(long id)
         {
-        }
-
-        public List<Person> FindAll()
-        {
-            var persons = new List<Person>();
-            for (int i = 0; i < 8; i++)
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
             {
-                var person = MockPerson(i);
-                persons.Add(person);
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
             }
-            return persons;
         }
 
-        private Person MockPerson(int i)
+        public bool Exists(long id)
         {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Person Name" + i,
-                LastName = "Person Last Name",
-                Address = "Some Address",
-                Genre = "Male"
-            };
-        }
-
-        public Person FindById(long id)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Eduardo",
-                LastName = "Fraga",
-                Address = "Porto Alegre - Rio Grande do Sul - Brasil",
-                Genre = "Male"
-            };
-        }
-
-        private long IncrementAndGet()
-        {
-            return Interlocked.Increment(ref count);
-        }
-
-        public Person Update(Person person)
-        {
-            return person;
+            return _context.Persons.Any(p => p.Id.Equals(id));
         }
     }
 }
