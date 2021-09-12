9f8bdf64ec48035bece471cd116ec7b133a9fb6b
Eduardo Antonio Fraga Chiamulera <eduardo.chiamulera@linx.com.br>
2021-09-12 16:43:18 -0300

    working with Custom Serialization

--- 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
+++ 02_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -1,15 +1,22 @@
-﻿namespace RestWithASPNETUdemy.Data.VO
+﻿using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
 {
     public class PersonVO
     {
+        [JsonPropertyName("code")]
         public long Id { get; set; }
 
+        [JsonPropertyName("name")]
         public string FirstName { get; set; }
 
+        [JsonPropertyName("last_name")]
         public string LastName { get; set; }
 
+        [JsonIgnore]
         public string Address { get; set; }
 
+        [JsonPropertyName("sex")]
         public string Genre { get; set; }
     }
 }
